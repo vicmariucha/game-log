728fa7622a8a12709b9508b9ef2f8506c6a66a80
pdr-rvr <rovirapedro@icloud.com>
2025-04-13 15:57:10 -0300

    login fix

--- app/src/main/java/com/example/gamelog/LoginActivity.java
+++ app/src/main/java/com/example/gamelog/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.gamelog;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -31,5 +32,8 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public void navigateToSignUp(View view) {
+        startActivity(new Intent(this, SignUpActivity.class));
     }
 }
